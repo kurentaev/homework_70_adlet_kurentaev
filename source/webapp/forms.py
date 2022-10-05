70dd3dd69ee5d21ec3f5a8dee7d1d449df9b9175
Adlet Kurentaev <kurentaev@gmail.com>
2022-10-05 15:45:25 +0600

    Создан Django-проект, применены первоначальные настройки.

---
+++ source/webapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import widgets
+from webapp.models import StatusChoices
+
+
+class TasksListForm(forms.Form):
+    title = forms.CharField(max_length=400, required=True, label='Title')
+    status = forms.ChoiceField(required=True, label='Status', choices=StatusChoices.choices)
+    deadline = forms.DateField(required=False,
+                               label='Deadline',
+                               widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+    description = forms.CharField(max_length=3000,
+                                  required=False,
+                                  label='Description',
+                                  widget=widgets.Textarea)
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 1:
+            raise ValidationError('The title must be longer than one characters')
+        return title
---
+++ source/webapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from webapp.views.tasks import add_view
+from webapp.views.base import index_view
+from webapp.views.tasks import task_view
+from webapp.views.tasks import delete_view
+from webapp.views.tasks import confirm_delete
+from webapp.views.tasks import update_view
+
+
+urlpatterns = [
+    path('', index_view, name='index'),
+    path('todo_list/add/', add_view, name='todo_add'),
+    path('todo_list/', index_view, name='index'),
+    path('todo_list/<int:pk>', task_view, name='todo_detail'),
+    path('delete_task/<int:pk>', delete_view, name='delete'),
+    path('todo_list/confirm_delete/<int:pk>', confirm_delete, name='confirm_delete'),
+    path('todo_list/update/<int:pk>', update_view, name='todo_update'),
+]
