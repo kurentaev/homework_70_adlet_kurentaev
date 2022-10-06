4389771ebde2df07bbfef61cbb3549820acc92d4
Adlet Kurentaev <kurentaev@gmail.com>
2022-10-06 10:15:49 +0600

    Добавлен просмотр одной задачи

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from webapp.views.base import IndexView
+from webapp.views.tasks import TaskView
 # from webapp.views.tasks import add_view
-# from webapp.views.tasks import task_view
 # from webapp.views.tasks import delete_view
 # from webapp.views.tasks import confirm_delete
 # from webapp.views.tasks import update_view
@@ -10,9 +10,9 @@ from webapp.views.base import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('todo_list/', IndexView.as_view(), name='home'),
+    path('todo_list/<int:pk>', TaskView.as_view(), name='todo_detail'),
     # path('todo_list/add/', add_view, name='todo_add'),
-    # path('todo_list/', index_view, name='home'),
-    # path('todo_list/<int:pk>', task_view, name='todo_detail'),
     # path('delete_task/<int:pk>', delete_view, name='delete'),
     # path('todo_list/confirm_delete/<int:pk>', confirm_delete, name='confirm_delete'),
     # path('todo_list/update/<int:pk>', update_view, name='todo_update'),
