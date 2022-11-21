bac3b933367a9c0a79ed4fbf366fdd4ce15f4dc4
Adlet Kurentaev <kurentaev@gmail.com>
2022-11-21 10:38:41 +0600

    Добавлена серилизатор для проектов

--- source/api/serializers.py
+++ source/api/serializers.py
@@ -1,9 +1,46 @@
 from rest_framework import serializers
 from webapp.models import Tasks
+from webapp.models import Projects
 
 
 class TasksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tasks
-        fields = ['id', 'summary', 'description', 'status', 'type', 'project', 'is_deleted' 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        fields = [
+            'id',
+            'summary',
+            'description',
+            'status',
+            'type',
+            'project',
+            'is_deleted',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = [
+            'id',
+            'created_at',
+            'updated_at'
+        ]
+
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = [
+            'id',
+            'start_date',
+            'end_date',
+            'summary',
+            'description',
+            'is_deleted',
+            'deleted_at',
+            'user',
+            'is_deleted_user',
+            'created_at'
+        ]
+        read_only_fields = [
+            'id',
+            'created_at',
+            'deleted_at'
+        ]
